8d524750ab178e741c9c5ec008d54064ad73a4a9
Ernesto Martinez <61639551+ErnestWarhead@users.noreply.github.com>
2024-02-26 18:53:28 +0000

    Solving fing webpack issues

--- src/js/app.js
+++ src/js/app.js
@@ -4,7 +4,7 @@ window.variables = {
   includeCover: true, // if includeCover is true the algorithm should show the cover image
   background: "https://images.unsplash.com/photo-1511974035430-5de47d3b95da", // this is the image's url that will be used as a background for the profile cover
   avatarURL: "https://randomuser.me/api/portraits/women/42.jpg", // this is the url for the profile avatar
-  socialMediaPosition: "right", // social media bar position (left or right)
+  socialMediaPosition: "position-right", // social media bar position (left or right)
 
   twitter: null, // social media usernames
   github: null,
@@ -28,14 +28,22 @@ function render(variables = {}) {
   document.querySelector("#widget_content").innerHTML = `<div class="widget">
             ${cover}
           <img src="${variables.avatarURL}" class="photo" />
-          <h1>Lucy Boilett</h1>
-          <h2>Web Developer</h2>
-          <h3>Miami, USA</h3>
-          <ul class="position-right">
-            <li><a href="https://twitter.com/4geeksacademy"><i class="fab fa-twitter"></i></a></li>
-            <li><a href="https://github.com/4geeksacademy"><i class="fab fa-github"></i></a></li>
-            <li><a href="https://linkedin.com/school/4geeksacademy"><i class="fab fa-linkedin"></i></a></li>
-            <li><a href="https://instagram.com/4geeksacademy"><i class="fab fa-instagram"></i></a></li>
+          <h1>${variables.name + "" + variables.lastName}</h1>
+          <h2>${variables.role}</h2>
+          <h3>${variables.city + "" + variables.country}</h3>
+          <ul class=${variables.socialMediaPosition}>
+            <li><a href="https://twitter.com/4geeksacademy"><i class="fa-brands fa-x-twitter"></i>${
+              variables.twitter
+            }</a></li>
+            <li><a href="https://github.com/4geeksacademy"><i class="fab fa-github"></i>${
+              variables.github
+            }</a></li>
+            <li><a href="https://linkedin.com/school/4geeksacademy"><i class="fab fa-linkedin"></i>${
+              variables.linkedin
+            }</a></li>
+            <li><a href="https://instagram.com/4geeksacademy"><i class="fab fa-instagram"></i>${
+              variables.instagram
+            }</a></li>
           </ul>
         </div>
     `;
@@ -53,17 +61,17 @@ window.onload = function() {
     // this is the url for the profile avatar
     avatarURL: "https://randomuser.me/api/portraits/women/42.jpg",
     // social media bar position (left or right)
-    socialMediaPosition: "position-left",
+    socialMediaPosition: "position-right",
     // social media usernames
     twitter: null,
     github: null,
     linkedin: null,
     instagram: null,
-    name: null,
-    lastName: null,
-    role: null,
-    country: null,
-    city: null
+    name: "Lucy",
+    lastName: "Boilett",
+    role: "Web Developer",
+    country: "USA",
+    city: "Miami"
   };
   render(window.variables); // render the card for the first time
 
